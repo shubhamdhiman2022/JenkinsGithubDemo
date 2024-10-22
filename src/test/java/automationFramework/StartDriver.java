c735dfa86e9db679e31454a6ddc865f77bd71468
shubham_dhiman_bitbucket <shubham.dhiman@grazitti.com>
2024-10-22 17:36:25 +0530

    test

--- src/test/java/automationFramework/StartDriver.java
+++ src/test/java/automationFramework/StartDriver.java
@@ -13,6 +13,8 @@ import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -27,7 +29,7 @@ public class StartDriver {
 	 */
 	public static void initializeWeb_Driver()
 			throws IOException, ParseException, org.json.simple.parser.ParseException, InterruptedException {
-		String browser = "chrome";
+		String browser = "firefox";
 		//DataReader.getParameterString("browser", "environment");
 		if ((browser.equalsIgnoreCase("chrome"))) {
 			WebDriverManager.chromedriver().setup();
@@ -61,12 +63,14 @@ public class StartDriver {
 		}
 
 		else if ((browser.equalsIgnoreCase("firefox"))) {
-			WebDriverManager.firefoxdriver().setup();
+			WebDriverManager.edgedriver().setup();
 			DesiredCapabilities caps = new DesiredCapabilities();
 			caps.setAcceptInsecureCerts(true);
-			FirefoxOptions options = new FirefoxOptions();
+			EdgeOptions options = new EdgeOptions();
+			//FirefoxOptions options = new FirefoxOptions();
 			options.addArguments("--start-maximized");
-			driver = new FirefoxDriver(options);
+			//driver = new FirefoxDriver(options);
+			driver = new EdgeDriver(options);
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(120));
 		}
